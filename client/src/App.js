333b1a5e20e33d40089945911716aef39e71664c
amjadsh92 <amjadsharafeddien@gmail.com>
2024-03-06 19:54:06 +0100

    updating App.js file: we fectch data using the custun functional hook useFetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
+import './App.css';
+import useFetch from "./useFetch.js";
+import React, {useEffect, useState} from "react"
 import axios from "axios";
 
 
-import './App.css';
-import useFetch from "./useFetch";
+export default function App() {
+  
+const{data,loading,error} = useFetch("http://localhost:3001/tasks")  
+  
+if (loading) return <h1>Loading</h1>
+
+if (error) console.log(error)
 
 
 
-function App() {
+return(<Credentials data={data}/>
+)  
 
-const {data, loading, error} = useFetch(url.com)
-
-return(
-<div className="App">
-  <ul >
-      <li>id: {data.id}</li>
-      <li>name: {data.name}</li>
-      <li>email: {data.description} </li>
-  </ul>
-
-
-
-</div>
-)
 
 }
 
-export default App;
+
+function Credentials({data}){
+  
+console.log("data", data)
+return (<div>
+<li>id:{data? data[0].id : "undefined"} </li>
+<li>name:{data? data[0].name : "undefined"}</li>
+<li>email:{data? data[0].email : "undefined"} </li>
+</div>)
+}
+
 
